50e52f89203890ea1fdcf1c4f32efb46d91b3b16
Dawon Jung <dwnee0@gmail.com>
2025-04-02 20:01:04 +0900

    to lesson 6

--- src/App.js
+++ src/App.js
@@ -11,32 +11,66 @@ import Box from "./component/Box"
 const choice = {
   rock:{
     name:"Rock",
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHHIiwU8uVoAk9ky8t15hYouYcWK5MANg7Ig&s"
+    img:"https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTEyL2pvYjk1OS1lbGVtZW50LWItMDEzNi14XzEuanBn.jpg"
   },
   scissors:{
     name:"Scissors",
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9DJ34PdDLPC1sWlhxbKizgHh3mdeDKGQT8w&s"
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbWcXvQWXfrxYNpNO3AhXdtzRrNbThKq3y-Q&s"
   },
   paper:{
     name:"Paper",
-    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLZvT7LN6M3cKdxHLjRP9MFHbWQwURDOZrZA&s"
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpALxO81BcGjGWz8HA0Q6j2zd0iqYGRHwRFQ&s"
   }
 }
 function App() {
   const [userSelect, setUserSelect] = useState(null)
+  const [computerSelect, setComputerSelect] = useState(null)
+  const [result,setResult] = useState("")
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]) 
+    let computerChoice = randomChoice()
+    setComputerSelect(computerChoice)
+    setResult(judgement(choice[userChoice],computerChoice))
+  };
+  const judgement = (user, computer) =>{
+    console.log("user : ", user,", computer : " ,computer)
+    // user === computer tie
+    // user === rock, computer=== scissors  : user win
+    // user === rock, computer === paper : user lose
+    // user === scissors, computer === paper : user win 
+    // user === scissors, computer === rock : user lose
+    // user === papers, computer === rock : user win 
+    // user === papers, computer === scissors : user lose 
+
+    if(user.name === computer.name){
+      return "tie"
+    } else if(user.name === "Rock") return computer.name==="Scissors"?"win":"lose"
+    else if(user.name === "Scissors") return computer.name==="Paper"?"win":"lose"
+    else if(user.name === "Paper") return computer.name==="Rock"?"win":"lose"
+  }
+
+  const randomChoice=()=>{
+    let itemArray = Object.keys(choice);
+    console.log("item array : ", itemArray)
+    let randomItem = Math.floor(Math.random()*itemArray.length);
+    let final = itemArray[randomItem]
+    console.log("final", final)
+    return choice[final]
   }
   return (
     <div>
+      <h1>가위바위보 게임</h1>
       <div className="main">
-        <Box title="You" item={userSelect|| { img: "https://i.namu.wiki/i/J9A5vBE6BuFBd09_MojiggkfWoci1CuuIjNf19nuSnxmfqmx4oYePJh0FWVzCmc5AIHxVoi2QWF8G5yIs_iwRA.webp" }}/>
-        {/* <Box title="Computer"/> */}
+        <Box title="You" item={
+          userSelect|| { img: "https://i.namu.wiki/i/J9A5vBE6BuFBd09_MojiggkfWoci1CuuIjNf19nuSnxmfqmx4oYePJh0FWVzCmc5AIHxVoi2QWF8G5yIs_iwRA.webp" }
+          } 
+          result={result}/>
+        <Box title="Computer" item={computerSelect|| { img: "https://i.namu.wiki/i/J9A5vBE6BuFBd09_MojiggkfWoci1CuuIjNf19nuSnxmfqmx4oYePJh0FWVzCmc5AIHxVoi2QWF8G5yIs_iwRA.webp"}} result={result}/>
       </div>
       <div className="main">
-        <button onClick={() => play("scissors")}>가위</button>
-        <button onClick={() => play("rock")}>바위</button>
-        <button onClick={() => play("paper")}>보</button>
+        <button onClick={() => play("scissors")}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbWcXvQWXfrxYNpNO3AhXdtzRrNbThKq3y-Q&s"/></button>
+        <button onClick={() => play("rock")}><img src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTEyL2pvYjk1OS1lbGVtZW50LWItMDEzNi14XzEuanBn.jpg"/></button>
+        <button onClick={() => play("paper")}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpALxO81BcGjGWz8HA0Q6j2zd0iqYGRHwRFQ&s"/></button>
       </div>
     </div>
   );
